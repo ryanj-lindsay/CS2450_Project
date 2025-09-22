463c88aa3a9e2b16a431515280076c527c010dcc
hayden-t-c <hayden.christenson@outlook.com>
2025-09-22 16:15:10 -0600

    Update test_control.py
    completed control test

--- tests/test_control.py
+++ tests/test_control.py
@@ -2,13 +2,35 @@ import pytest
 from uvsim import *
 
 def test_branch():
-  pass
+  sim = UVSim()
+  sim.memory[0] = 77
+  sim.load([4002, 4300, 2000, 4300])
+  sim.run()
+
+  assert sim.accumulator == 77
 
 def test_branchneg():
-  pass
+  sim = UVSim()
+  sim.memory[0] = -5
+  sim.memory[1] = 111
+  sim.load([2000, 4104, 4300, 2001, 4300])
+  sim.run()
+
+  assert sim.accumulator == 111
 
 def test_branchzero():
-  pass
+  sim = UVSim()
+  sim.memory[0] = 0 
+  sim.memory[1] = 222
+  sim.load([2000, 4204, 4300, 2001, 4300])
+  sim.run()
+
+  assert sim.accumulator == 222
 
 def test_halt():
-  pass
+  sim = UVSim()
+  sim.memory[0] = 9
+  sim.load([2000, 4300, 3000])
+  sim.run()
+
+  assert sim.accumulator == 9 
