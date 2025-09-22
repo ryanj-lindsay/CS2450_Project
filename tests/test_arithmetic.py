8157cd998a6afc1d512df9473f02e9674ed6e400
hayden-t-c <hayden.christenson@outlook.com>
2025-09-22 16:08:14 -0600

    Update test_arithmetic.py
    Completed arithmetic test

--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -2,16 +2,46 @@ import pytest
 from uvsim import *
 
 def test_add():
-  pass
+  sim = UVSim()
+  sim.memory[0] = 10
+  sim.memory[1] = 5
+  sim.load([2000, 3001, 4300])
+  sim.run()
+
+assert sim.accumulator == 15
 
 def test_subtract():
-  pass
+  sim = UVSim()
+  sim.memory[0] = 10
+  sim.memory[1] = 5
+  sim.load([2000, 3101, 4300])
+  sim.run()
+
+  assert sim.accumulator == 5
 
 def test_divide():
-  pass
+  sim = UVSim()
+  sim.memory[0] = 12
+  sim.memory[1] = 4
+  sim.load([2000, 3201, 4300])
+  sim.run()
+
+  assert sim.accumulator == 3
 
 def test_divide_by_zero():
-  pass
+  sim = UVSim()
+  sim.memory[0] = 12
+  sim.memory[1] = 0
+  sim.load([2000, 3201, 4300])
+  
+  with pytest.raises(ZeroDivisionError):
+    sim.run()
 
 def test_multiply():
-  pass
+  sim = UVSim()
+  sim.memory[0] = 3
+  sim.memory[1] = 4
+  sim.load([2000, 3301, 4300])
+  sim.run()
+  
+  assert sim.accumulator == 12
