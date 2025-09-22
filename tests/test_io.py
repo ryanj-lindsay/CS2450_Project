17e29ba6e5747245520f635059b6da78cf013ea9
hayden-t-c <hayden.christenson@outlook.com>
2025-09-22 15:58:51 -0600

    Update test_io.py
    Completed io test file.

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,11 +1,27 @@
 import pytest
 from uvsim import *
 
-def load_file(inputs = None):
-  pass
+def load_file(file):
+  with open(f"tests/{file}", "r") as f:
+    return [line.strip() for line in f if line.strip()]
 
 def test_read():
-  pass
+  inputs = [5, 7]
+  outputs = []
+  sim = UVSim(input_function = lambda: inputs.pop(0), output_function = lambda x: outputs.append(x))
+  program = load_file("Test1.txt")
+  sim.load(program)
+  sim.run()
+
+  assert sim.memory[7] == 5
+  assert sim.memory[8] == 7
 
 def test_write():
-  pass
+  inputs = [5, 7]
+  outputs = []
+  sim = UVSim(input_function = lambda: inputs.pop(0), output_function = lambda x: outputs.append(x))
+  program = load_file("Test1.txt")
+  sim.load(program)
+  sim.run()
+
+  assert outputs[-1] == sim.memory[9]
