14bc90239befbc82fcd3364da1eabada2e67441a
hayden-t-c <hayden.christenson@outlook.com>
2025-09-22 15:36:27 -0600

    Create test_arithmetic.py
    Arithmetic test.

---
+++ test_arithmetic.py
@@ -0,0 +1,17 @@
+import pytest
+from uvsim import *
+
+def test_add():
+  pass
+
+def test_subtract():
+  pass
+
+def test_divide():
+  pass
+
+def test_divide_by_zero():
+  pass
+
+def test_multiply():
+  pass
