583a761b028a6852fd56221456b8d13a76bd40d5
hayden-t-c <hayden.christenson@outlook.com>
2025-10-03 20:56:49 -0600

    Update test_control.py

--- tests/test_control.py
+++ tests/test_control.py
@@ -2,35 +2,47 @@ import pytest
 from uvsim import *
 
 def test_branch():
-  sim = UVSim()
-  sim.memory[0] = 77
-  sim.load([4002, 4300, 2000, 4300])
-  sim.run()
-
-  assert sim.accumulator == 77
+    sim = UVSim()
+    sim.memory[0] = 77
+    sim.load([4002, 4300, 2000, 4300])
+    sim.run()
+    assert sim.accumulator == 77
 
 def test_branchneg():
-  sim = UVSim()
-  sim.memory[0] = -5
-  sim.memory[1] = 111
-  sim.load([2000, 4104, 4300, 2001, 4300])
-  sim.run()
+    sim = UVSim()
+    sim.memory[0] = -5
+    sim.memory[1] = 111
+    sim.load([2000, 4104, 4300, 2001, 4300])
+    sim.run()
+    assert sim.accumulator == 111
 
-  assert sim.accumulator == 111
+def test_branchneg_no_branch():
+    sim = UVSim()
+    sim.memory[0] = 5
+    sim.memory[1] = 111
+    sim.load([2000, 4104, 2001, 4300])
+    sim.run()
+    assert sim.accumulator == 111
 
 def test_branchzero():
-  sim = UVSim()
-  sim.memory[0] = 0 
-  sim.memory[1] = 222
-  sim.load([2000, 4204, 4300, 2001, 4300])
-  sim.run()
+    sim = UVSim()
+    sim.memory[0] = 0
+    sim.memory[1] = 222
+    sim.load([2000, 4204, 4300, 2001, 4300])
+    sim.run()
+    assert sim.accumulator == 222
 
-  assert sim.accumulator == 222
+def test_branchzero_no_branch():
+    sim = UVSim()
+    sim.memory[0] = 7
+    sim.memory[1] = 222
+    sim.load([2000, 4204, 2001, 4300])
+    sim.run()
+    assert sim.accumulator == 222
 
 def test_halt():
-  sim = UVSim()
-  sim.memory[0] = 9
-  sim.load([2000, 4300, 3000])
-  sim.run()
-
-  assert sim.accumulator == 9 
+    sim = UVSim()
+    sim.memory[0] = 9
+    sim.load([2000, 4300, 3000])
+    sim.run()
+    assert sim.accumulator == 9
